4adc5d3d707e244a83c7b42cd1c2c54777d1b523
Francis Chung <francis.chung@bondexchange.com.au>
2020-07-27 21:27:35 +1000

    Setup/Teardown for PhoneBookTest

--- test_phonebook.py
+++ test_phonebook.py
@@ -5,18 +5,21 @@ from phonebook import PhoneBook
 
 class PhoneBookTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.phonebook = PhoneBook()
+
+    def tearDown(self) -> None:
+        pass
+
     def test_lookup_by_name(self):
-        phonebook = PhoneBook()
-        phonebook.add("Bob", "12345")
-        number = phonebook.lookup("Bob")
+        self.phonebook.add("Bob", "12345")
+        number = self.phonebook.lookup("Bob")
         self.assertEqual("12345", number)
 
     def test_missing_name(self):
-        phonebook = PhoneBook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("MissingName")
+            self.phonebook.lookup("MissingName")
 
     @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
-        phonebook = PhoneBook()
-        self.assertTrue(phonebook.is_consistent())
+        self.assertTrue(self.phonebook.is_consistent())
